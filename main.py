ed798f0618fa7f9be6aeddd978fe48b45100ce2e
Beatriz Faro Carrera <farocarrera.beatriz@gmail.com>
2021-12-16 13:53:39 +0100

    def paused

--- main.py
+++ main.py
@@ -7,27 +7,34 @@
         cambiar direcciones
                 --> astronauta debe caer
                 --> objetos moverse de lado
-        PANTALLA inicial para empezar --> https://pythonprogramming.net/pause-game-pygame/
-        PANTALLA con game over y puntuación -^
+        PANTALLA inicial para empezar
+        PANTALLA con game over con boton seguir jugando y guardar su puntuación
+        PANTALLA pausa con botones seguir jugando o salir del juego --> guardando su puntuación
 '''
 
 import os
 import time
+from signal import pause  # ¿? probar si funciona
 
 import pygame, sys, random
+from pygame import K_p
+
 import constantes
 from Objetos import Objeto
 from jugador import Jugador
+
 
 def inicializar():
     pygame.init()
     # titulo ventana
     pygame.display.set_caption("STRATOS")
 
+
 def crearEventoIncrementarVelocidad():
     INC_SPEED = pygame.USEREVENT + 1
     pygame.time.set_timer(INC_SPEED, 1000)
     return INC_SPEED
+
 
 def finJuego(all_sprites):
     pygame.display.update()
@@ -37,7 +44,8 @@ def finJuego(all_sprites):
     pygame.quit()
     sys.exit()
 
-def moverFondo(screen,fondo,velocidad, y):
+
+def moverFondo(screen, fondo, velocidad, y):
     rel_y = y % fondo.get_rect().height
     screen.blit(fondo, (0, rel_y - fondo.get_rect().height))
     if rel_y < constantes.SCREEN_WIDTH:
@@ -45,7 +53,8 @@ def moverFondo(screen,fondo,velocidad, y):
     y -= 1 * velocidad  # fondo mas velocidad
     return y
 
-#código para GAME OVER
+
+# código para GAME OVER
 def gameOver(screen):
     WHITE = (255, 255, 255)
     gameOverFont = pygame.font.SysFont('arial.ttf', 54)  # Fuente y tamaño final del juego
@@ -54,30 +63,52 @@ def gameOver(screen):
     gameOverRect.midtop = (400, 300)  # posición de visualización
     screen.blit(gameOverSurf, gameOverRect)
 
-#código para pantalla de pausa
-def paused(screen, WHITE, button):
-    red = (255,0,0) #203,50,52
-    green = (0,143,57)
-    pausedFont = pygame.font.SysFont("arial.ttf", 54)
-    pausedSurf = pausedFont.render("PAUSED", True, WHITE)
-    pausedRect = pausedSurf.get_rect()
-    pausedRect.midtop = (400, 300) #((ancho_de_pantalla / 2), (altura_de_pantalla / 2))
-    screen.blit(pausedSurf, pausedRect)
 
-    while paused:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-    # gameDisplay.fill (blanco)
-        button("Continuar", 150, 450, 100, 50, verde, bright_green, Reanudar)
-        button("Salir", 550, 450, 100, 50, rojo, bright_red, gameOver(screen))
+# código para pantalla de pausa con pulsacion de tecla p para pausar-reanudar
+# Si quieres que dure hasta que vuelvas a pulsar p haz
+# un while True: que espere a que vuelvas a pulsar p
+def paused(screen):
+    WHITE = (255, 255, 255)
+    key = pygame.key.get_pressed()
+    if key == [K_p]:
+        red = (203, 50, 52)  # 203,50,52-->rojo más suave  #255,0,0-->rojo puro
+        green = (0, 143, 57)
+        pausedFont = pygame.font.SysFont("arial.ttf", 54)  # fuente para texto PAUSA
+        pausedSurf = pausedFont.render("PAUSA", True, WHITE)  # PAUSA en display
+        pausedRect = pausedSurf.get_rect()
+        pausedRect.midtop = (400, 300)  # ((ancho_de_pantalla / 2), (altura_de_pantalla / 2))
+        screen.blit(pausedSurf, pausedRect)
 
         pygame.display.update()
 
+# otra forma de pausar hasta que el usuario presione una tecla
+os.system("pause")
+
+''' red = (203,50,52) #203,50,52-->rojo más suave  #255,0,0-->rojo puro
+ green = (0,143,57)
+ pausedFont = pygame.font.SysFont("arial.ttf", 54) #fuente para texto PAUSA
+ pausedSurf = pausedFont.render("PAUSA", True, WHITE) #PAUSA en display
+ pausedRect = pausedSurf.get_rect()
+ pausedRect.midtop = (400, 300) #((ancho_de_pantalla / 2), (altura_de_pantalla / 2))
+ screen.blit(pausedSurf, pausedRect)
+
+ while pause:
+     for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+             pygame.quit()
+             sys.exit()
+
+ # gameDisplay.fill (blanco)
+
+     #botones continuar jugando o salir del juego
+     button("Continuar", 150, 450, 100, 50, green, unpaused)
+     button("Salir", 550, 450, 100, 50, red, gameOver(screen))
+
+     pygame.display.update()'''
 
 '''MAIN DEL JUEGO'''
+
+
 def main():
     inicializar()
     screen = pygame.display.set_mode((constantes.SCREEN_WIDTH, constantes.SCREEN_HEIGHT))
@@ -92,38 +123,38 @@ def main():
     piedra = Objeto()
 
     ''' creacion de grupos de sprites, para despues poder colisionar'''
-    #grupo que contiene todos los elementos
+    # grupo que contiene todos los elementos
     all_sprites = pygame.sprite.Group()
     all_sprites.add(astronauta)
     all_sprites.add(piedra)
-    #grupo que contiene los enemigos
+    # grupo que contiene los enemigos
     enemies = pygame.sprite.Group()
     enemies.add(piedra)
 
     EVENT_INC_SPEED = crearEventoIncrementarVelocidad()
-    velocidad = constantes.speed #inicializamos velocidad desde constante
+    velocidad = constantes.speed  # inicializamos velocidad desde constante
     y = 0
 
     while True:
-        #screen.blit(fondo, (0, 0))
+        # screen.blit(fondo, (0, 0))
 
         # control de eventos
         for event in pygame.event.get():
-            #si se produce el evento de incrementar velocidad le sumammos
+            # si se produce el evento de incrementar velocidad le sumammos
             if event.type == EVENT_INC_SPEED:
                 velocidad += 1
 
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        #mover fondo en vertical
+        # mover fondo en vertical
         y = moverFondo(screen, fondo, velocidad, y)
 
         for entity in all_sprites:
             screen.blit(entity.image, entity.rect)
             entity.mover()
 
-        #si colisiona astronauta con algun enemigo fin del juego con game over
+        # si colisiona astronauta con algun enemigo fin del juego con game over
         if pygame.sprite.spritecollideany(astronauta, enemies):
             gameOver(screen)
             finJuego(all_sprites)
@@ -131,6 +162,6 @@ def main():
         pygame.display.update()
         FramePerSec.tick(constantes.FPS)
 
+
 if __name__ == '__main__':
     main()
-
